d6de89dd0962c2b2a305a09663789ac965569a7f
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-06-19 15:16:04 +0300

    Update and rename WordTest.java to LettersInWordReplacementTest.java

--- src/test/java/ua/lviv/iot/algo/part1/lab5/LettersInWordReplacementTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab5/LettersInWordReplacementTest.java
@@ -1,31 +1,33 @@
 package ua.lviv.iot.algo.part1.lab5;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.Test;
 
-public class WordTest {
+public class LettersInWordReplacementTest {
 	@Test
 	public void testSwapWordsWithEmptyInput() {
+		final LettersInWordReplacement word = new LettersInWordReplacement();
 	    String input = "";
 	    String expectedOutput = "";
-	    String actualOutput = Word.swapWords(input);
+	    String actualOutput = word.swapWords(input);
 	    assertEquals(expectedOutput, actualOutput);
 	}
 
 	@Test
 	public void testSwapWords() {
+		final LettersInWordReplacement word = new LettersInWordReplacement();
 	    String input = "its a longest word in a whole world.";
 	    String expectedOutput = "longest a its word in a whole world.";
-	    String actualOutput = Word.swapWords(input);
+	    String actualOutput = word.swapWords(input);
 	    assertEquals(expectedOutput, actualOutput);
 	}
 	
 	@Test
     public void testSwapWordsWithLongWords() {
-        String text = "Supercalifragilisticexpialidocious this element is long too.";
-        String expected = "Supercalifragilisticexpialidocious this element is long too.";
-        String result = Word.swapWords(text);
+		final LettersInWordReplacement word = new LettersInWordReplacement();
+        String text = "element is too long Supercalifragilisticexpialidocious.";
+        String expected = "Supercalifragilisticexpialidocious is too long element.";
+        String result = word.swapWords(text);
         assertEquals(expected, result);
     }
 }
